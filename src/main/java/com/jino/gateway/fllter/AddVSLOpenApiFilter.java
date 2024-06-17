aab89c843a950ad94bd37ee677339802a816eea0
jinoh Yang <jino7485@nate.com>
2024-06-17 16:26:18 +0900

    <feat>: add Route
    Route to Open API(VSL)

--- src/main/java/com/jino/gateway/fllter/AddVSLOpenApiFilter.java
+++ src/main/java/com/jino/gateway/fllter/AddVSLOpenApiFilter.java
@@ -7,26 +7,37 @@ import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFac
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @Slf4j
 @Component
-public class AddKakaoOpenApiKeyFilter extends AbstractGatewayFilterFactory<AddKakaoOpenApiKeyFilter.Config> {
+public class AddVSLOpenApiFilter extends AbstractGatewayFilterFactory<AddVSLOpenApiFilter.Config> {
 
-    @Value("${kakao.api-key}")
+    @Value("${vslOpenApi.api-key}")
     private String apiKey;
 
-    public AddKakaoOpenApiKeyFilter() {
-        super(AddKakaoOpenApiKeyFilter.Config.class);
+    public AddVSLOpenApiFilter() {
+        super(AddVSLOpenApiFilter.Config.class);
     }
-
     @Override
-    public GatewayFilter apply(AddKakaoOpenApiKeyFilter.Config config) {
+    public GatewayFilter apply(AddVSLOpenApiFilter.Config config) {
         return (exchange, chain) -> {
+
+            URI uri = exchange.getRequest().getURI();
+            URI newUri = UriComponentsBuilder.fromUri(uri)
+                    .queryParam("apiKey", apiKey)
+                    .queryParam("getType", "json")
+                    .build(true)
+                    .toUri();
+
             ServerHttpRequest request = exchange.getRequest().mutate()
-                    .header("kakao-open-key", apiKey)
+                    .uri(newUri)
                     .build();
+
             ServerWebExchange modifiedExchange = exchange.mutate().request(request).build();
+
             return chain.filter(modifiedExchange);
         };
     }
@@ -34,4 +45,5 @@ public class AddKakaoOpenApiKeyFilter extends AbstractGatewayFilterFactory<AddKa
     public static class Config {
         // Put configuration properties here if needed
     }
+
 }
--- src/main/java/com/jino/gateway/fllter/LoggingFilter.java
+++ src/main/java/com/jino/gateway/fllter/LoggingFilter.java
@@ -3,6 +3,7 @@ package com.jino.gateway.fllter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -26,15 +27,25 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
     private void logRequest(ServerWebExchange exchange) {
         log.info("Request Method: {}", exchange.getRequest().getMethod());
         log.info("Request URL: {}", exchange.getRequest().getURI());
-        exchange.getRequest().getHeaders().forEach((name, values) ->
-                values.forEach(value -> log.info("Request Header: {}={}", name, value))
+//        exchange.getRequest().getHeaders().forEach((name, values) ->
+//                values.forEach(value -> log.info("Request Header: {}={}", name, value))
+//        );
+        exchange.getRequest().getQueryParams().forEach((name, values) ->
+                values.forEach(value -> log.info("Request Params: {}={}", name, value))
         );
     }
 
     private void logResponse(ServerWebExchange exchange) {
+//        log.info(exchange.getRequest().getURI().toString());
+//        log.info(exchange.getRequest().getQueryParams().toString());
+//        log.info(exchange.getRequest().getLocalAddress().toString());
+//        log.info(exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR).toString());
         exchange.getResponse().getHeaders().forEach((name, values) ->
                 values.forEach(value -> log.info("Response Header: {}={}", name, value))
         );
+        log.info(exchange.getResponse().getStatusCode().toString());
+        log.info("Response : " + exchange.mutate().response(exchange.getResponse()).build());
+
     }
 
     public static class Config {
