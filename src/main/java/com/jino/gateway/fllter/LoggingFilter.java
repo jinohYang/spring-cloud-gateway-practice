7ce69a749e89dc949b30feb994428eac114fd434
jinoh Yang <jino7485@nate.com>
2024-06-20 20:01:18 +0900

    <build>: change java version
    Change java version 17 -> 21

--- src/main/java/com/jino/gateway/fllter/LoggingFilter.java
+++ src/main/java/com/jino/gateway/fllter/LoggingFilter.java
@@ -3,7 +3,6 @@ package com.jino.gateway.fllter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -27,19 +26,12 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
     private void logRequest(ServerWebExchange exchange) {
         log.info("Request Method: {}", exchange.getRequest().getMethod());
         log.info("Request URL: {}", exchange.getRequest().getURI());
-//        exchange.getRequest().getHeaders().forEach((name, values) ->
-//                values.forEach(value -> log.info("Request Header: {}={}", name, value))
-//        );
         exchange.getRequest().getQueryParams().forEach((name, values) ->
                 values.forEach(value -> log.info("Request Params: {}={}", name, value))
         );
     }
 
     private void logResponse(ServerWebExchange exchange) {
-//        log.info(exchange.getRequest().getURI().toString());
-//        log.info(exchange.getRequest().getQueryParams().toString());
-//        log.info(exchange.getRequest().getLocalAddress().toString());
-//        log.info(exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR).toString());
         exchange.getResponse().getHeaders().forEach((name, values) ->
                 values.forEach(value -> log.info("Response Header: {}={}", name, value))
         );
